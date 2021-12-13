f8c260c06e0a3d36c777f216e041459ba82d748c
Mateo Barría <mateobarria@gmail.com>
2021-12-13 18:53:04 -0300

    Changed how .csv files are copied (now skips comments)

--- scripts/copy_files.py
+++ scripts/copy_files.py
@@ -6,25 +6,39 @@ from glob import glob
 from shutil import copy
 from os import makedirs
 
+def copycsv(original, new):
+    # to use with latex we need to remove the first line (comment)
+    with open(original, 'r') as io:
+        lines = io.readlines()
+    with open(new, 'w') as io:
+        for line in lines:
+            if line[0] != '#':
+                io.write(line)
+
 target = "/home/mbarria/Dropbox/Papers/graphene_adsorption_paper/data"
 # 1.- Tables
 makedirs(f"{target}/tables", exist_ok=True)
 source = "/home/mbarria/don-elias/AA_GO0US/tables/2021_12_10_SASA"
 
-copy(f"{source}/Table1.csv", f"{target}/tables/Free_Energy_Decomposition.csv")
-copy(f"{source}/Table2.csv", f"{target}/tables/Energy_Decomposition.csv")
-copy(f"{source}/Table3.csv", f"{target}/tables/Internal_and_External_Energy.csv")
-copy(f"{source}/Table4.csv", f"{target}/tables/Entropy_and_Diffusion.csv")
-copy(f"{source}/Table5.csv", f"{target}/tables/SASA.csv")
+copycsv(f"{source}/Table1.csv", f"{target}/tables/Free_Energy_Decomposition.csv")
+copycsv(f"{source}/Table2.csv", f"{target}/tables/Energy_Decomposition.csv")
+copycsv(f"{source}/Table3.csv", f"{target}/tables/Internal_and_External_Energy.csv")
+copycsv(f"{source}/Table4.csv", f"{target}/tables/Entropy_and_Diffusion.csv")
+copycsv(f"{source}/Table5.csv", f"{target}/tables/SASA.csv")
 
 # 25%
 source = "/home/mbarria/don-elias1/AA_GO25US/tables/2021_12_09_SASA"
 
-copy(f"{source}/Table1.csv", f"{target}/tables/Free_Energy_Decomposition_25o.csv")
-copy(f"{source}/Table2.csv", f"{target}/tables/Energy_Decomposition_25o.csv")
-copy(f"{source}/Table3.csv", f"{target}/tables/Internal_and_External_Energy_25o.csv")
-copy(f"{source}/Table4.csv", f"{target}/tables/Entropy_and_Diffusion_25o.csv")
-copy(f"{source}/Table5.csv", f"{target}/tables/SASA_25o.csv")
+copycsv(f"{source}/Table1.csv", f"{target}/tables/Free_Energy_Decomposition_25o.csv")
+copycsv(f"{source}/Table2.csv", f"{target}/tables/Energy_Decomposition_25o.csv")
+copycsv(f"{source}/Table3.csv", f"{target}/tables/Internal_and_External_Energy_25o.csv")
+copycsv(f"{source}/Table4.csv", f"{target}/tables/Entropy_and_Diffusion_25o.csv")
+copycsv(f"{source}/Table5.csv", f"{target}/tables/SASA_25o.csv")
+
+# Helix 5-Ala
+
+# Stretched 5-Ala
+
 
 # 2.- PMFs
 makedirs(f"{target}/pmfs", exist_ok=True)
